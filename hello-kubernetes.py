badff5ae0340cd22e0487198fa7437cc4c1374ad
Shihab Hasan <shihabhasan.iut@gmail.com>
2023-11-28 11:00:15 -0600

    adding new task for pi estimation

--- hello-kubernetes.py
+++ hello-kubernetes.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+import random
+import math
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
@@ -20,6 +22,24 @@ dag = DAG(
 def print_hello():
     print("Hello, Kubernetes!")
 
+def  estimate_pi():
+    num_points = 1000000
+    points_inside_circle = 0
+
+    for _ in range(num_points):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+
+        distance = math.sqrt(x**2 + y**2)
+
+        if distance <= 1:
+            points_inside_circle += 1
+
+    pi_estimate = (points_inside_circle / num_points) * 4
+    
+    print("Calculated pi:")
+    print(pi_estimate)
+
 # Create a task using the PythonOperator
 task_hello = PythonOperator(
     task_id='print_hello_task',
@@ -27,6 +47,13 @@ task_hello = PythonOperator(
     dag=dag,
 )
 
+# Create a task using the PythonOperator
+task_pi = PythonOperator(
+    task_id='estimate_pi_task',
+    python_callable=estimate_pi,
+    dag=dag,
+)
+
 # Set task dependencies
-task_hello
+task_hello >> task_pi
 
